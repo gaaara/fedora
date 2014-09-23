36f372a37c372434a6eda08edd58de4013d07cfd
gaaara <enixdarky2@gmail.com>
2014-09-23 14:35:29 -0500

    Create Flash-install.sh

---
+++ Flash-install.sh
@@ -0,0 +1,29 @@
+#test script
+#!/bin/bash
+     
+    ## Adobe Repository 64-bit x86_64 ##
+    rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm
+    rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+     
+    yum install devilspie flash-plugin
+     
+    su -c " mkdir ~/.devilspie" gaaara
+    su -c "touch ~/.devilspie/flash-fullscreen-firefox.ds" gaaara
+     
+    su -c "cat <<'EOF' > ~/.devilspie
+    (if
+    (is (application_name) "plugin-container")
+    (begin
+    (focus)
+    )
+    )
+    EOF
+    " gaaara
+     
+    cat <<'EOF' > /etc/rc.local/devilspie0032.sh
+    #!/bin/sh
+    /usr/bin/devilspie
+    EOF
+     
+    chmod r+x /etc/rc.local/devilspie0032.sh
+
